46f76642c9807882f005ce1d60ea9ce21249f152
bridgewalker <thilo2gross@gmail.com>
2020-09-12 17:46:45 +0200

    Setting Up

--- src/QuickNet.jl
+++ src/QuickNet.jl
@@ -4,6 +4,12 @@ using LightGraphs: AbstractGraph
 
 include("structure.jl")
 
+function QNet(N::Int,K::Int)
+      firststub=zeroes(Int64,N)
+      stubs+Vector{Stub}(undef,2*K)
+      return QNet(stubs,firststub)
+end
+
 
 
 
