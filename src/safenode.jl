d5a608ecd3babfbbbc003de0e9ef9016abb48b03
Thilo Gross <69499588+bridgewalker@users.noreply.github.com>
2021-08-30 09:54:47 +0200

    Add files via upload

--- src/safenode.jl
+++ src/safenode.jl
@@ -675,50 +675,6 @@ function randomnode(net::FastNet,cls::Union{Array,Tuple})
 end
 
 """
-    randomnode(net)
-    randomnode(net,s)
-    randomnode_f(net)
-    randomnode_f(net,s)
-
-Return the id of a random node ddrawn from *net*.
-
-If the second argument *s* is not provided the node will be drawn uniformly from 
-all nodes in the network. If *s* is an integer then the node will be drawn uniformly 
-from the nodes in state *s*. If *s* is an Array or Tuple of Ints then the node will be 
-drawn uniformly from the nodes in the states listed. 
-
-This function runs in constant time if *s* is integer or omitted. If *s* is an Array or Tuple the 
-worst case performance scales only with the number of node states.  
-The fast (_f) verions sacrifice some safty checks for better performance. 
-See [basic concepts](concepts.md) for details. 
-
-The safe versions of this function will throw an ArgumentError with an informative error message
-when trying to pick a node from an empty set. With the fast (_f) version, trying to pick a node from 
-an empty set will also result in an ArgumentError being thrown, but in this case the message will be 
-something like "Range must be non-empty".  
-
-See also [node](#Fastnet.node)
-
-# Examples 
-```jldoctest
-julia> using Fastnet
-
-julia> net=FastNet(1000,2000,2,[])
-Network of 0 nodes and 0 links
-
-julia> makenodes!(net,100,1);
-
-julia> net
-Network of 100 nodes and 0 links
-```
-"""
-function nodestate(net::FastNet,nid)
-    task="Trying to determine state of node $nid,"
-    checknodeid(net,nid,task)
-    nodestate_f(net,nid)
-end
-
-"""
     adjacent(net,a,b)
     adjacent_f(net,a,b)
 
